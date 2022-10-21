b7670285bfacbbe2679a4a0b6f270559b4ca534f
pdemeu <pdemeu@outlook.com>
2022-10-20 21:42:28 -0300

    Reset project

---
+++ resolucoes/desafio1-pdemeu/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      sup
+    </div>
+  )
+}
+
+export default App
