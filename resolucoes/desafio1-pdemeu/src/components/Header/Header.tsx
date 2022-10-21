e71498c84d3aa99140624ae2c9f62f6b90f1f221
pdemeu <pdemeu@outlook.com>
2022-10-21 00:32:43 -0300

    Feat: list blog posts

--- resolucoes/desafio1-pdemeu/src/components/Header/Header.tsx
+++ resolucoes/desafio1-pdemeu/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from "./styles.module.css";
 
-function Header() {
+export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
@@ -10,5 +10,3 @@ function Header() {
     </header>
   )
 }
-
-export default Header
--- resolucoes/desafio1-pdemeu/src/pages/posts/index.tsx
+++ resolucoes/desafio1-pdemeu/src/pages/posts/index.tsx
@@ -1,8 +1,21 @@
-import Head from 'next/head'
 import React from 'react'
+import { GetStaticProps } from 'next';
+import Head from 'next/head'
+import { getPrismicClient } from '../../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from "prismic-dom"
 import styles from "./styles.module.css";
 
-function index() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+interface PostsProps {
+  posts: Post[]
+}
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -11,37 +24,47 @@ function index() {
 
       <main className={styles.container}>
         <div className={styles.list}>
-          <a href="">
-            <time>20 de outubro de 2022</time>
-            <strong>Lorem Ipsum</strong>
-            <p>
-              Lorem IpsumLorem Ipsum. Lorem Ipsum, Lorem IpsumLorem IpsumLorem Ipsum
-              Lorem Ipsum, Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum,
-              Lorem IpsumLorem Ipsum.
-            </p>
-          </a>
-          <a href="">
-            <time>20 de outubro de 2022</time>
-            <strong>Lorem Ipsum</strong>
-            <p>
-              Lorem IpsumLorem Ipsum. Lorem Ipsum, Lorem IpsumLorem IpsumLorem Ipsum
-              Lorem Ipsum, Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum,
-              Lorem IpsumLorem Ipsum.
-            </p>
-          </a>
-          <a href="">
-            <time>20 de outubro de 2022</time>
-            <strong>Lorem Ipsum</strong>
-            <p>
-              Lorem IpsumLorem Ipsum. Lorem Ipsum, Lorem IpsumLorem IpsumLorem Ipsum
-              Lorem Ipsum, Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum,
-              Lorem IpsumLorem Ipsum.
-            </p>
-          </a>
+          {posts.map(post => (
+            <a href="" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   )
 }
 
-export default index
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at("document.type", "post")
+  ], {
+    fetch: ["post.title", "post.content"],
+    pageSize: 100,
+  })
+
+  const posts = response.results.map(post => {
+    return {
+      flug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  console.log(JSON.stringify(response, null, 2))
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
